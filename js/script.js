9412f6de871d76bc9acf3ef698c3407544c0f804
TelitchkoV <telitchkovkw@gmail.com>
2022-04-26 00:45:55 +0500

    26-04-2022-window2

--- js/script.js
+++ js/script.js
@@ -69,3 +69,62 @@ window.addEventListener('keydown', e => {
 		}
 	})
 })
+
+
+
+
+// --------------------------------------------------------
+
+// Кнопка2
+
+
+var tel = document.getElementById('tel');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(tel, maskOptions);
+
+
+
+
+const modalBtns2 = document.querySelectorAll('._modal-open2');
+const modals2 = document.querySelectorAll('._modal2');
+
+const body2 = document.body;
+
+function openModal2(elem) {
+	elem.classList.add('_active');
+	body2.classList.add('_locked')
+}
+
+function closeModal2(e) {
+	if (e.target.classList.contains('modal-bg2')) {
+		e.target.closest('._modal2').classList.remove('_active');
+		body2.classList.remove('_locked')
+	}
+}
+
+modalBtns2.forEach(btn => {
+	btn.addEventListener('click', (e) => {
+		let data = e.target.dataset.modalOpen;
+
+		modals2.forEach(modal => {
+			if (modal.dataset.modal == data || modal.dataset.modal == e.target.closest('._modal-open2').dataset.modalOpen) {
+				openModal2(modal)
+			}
+		})
+	})
+})
+
+modals2.forEach(modal => {
+	modal.addEventListener('click', e => closeModal2(e))
+})
+
+window.addEventListener('keydown', e => {
+	modals2.forEach(modal => {
+		if (e.key === "Escape" && modal.classList.contains('_active')) {
+			modal.classList.remove('_active');
+			body2.classList.remove('_locked');
+		}
+	})
+})
